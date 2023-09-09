5349eb0d1e3cbcc78e103446980ac429c16ec0db
Drew Anello <drew.anello@gmail.com>
2023-09-09 18:18:37 -0400

    brew formula

---
+++ pwg.rb
@@ -0,0 +1,17 @@
+Class Pwg < Formula
+
+
+desc "PWG is a random 12 character long password generator"
+
+url "https://github.com/drew-anello/random-pw-generator/releases/download/v1.0.0/pwg-v1.0.0.tar.gz"
+sha256 "21fdd49137e6a2db24c7e65aa8bfb132cdbabd9d8b31f03b60b29a9dbe478ce2"
+
+depends_on "go" => :build
+
+def install
+  system "go", "build", *std_go_args
+end
+
+test do
+  assert_mathch "Version #{version}", shell_output("#{bin}/pwg version")
+end
