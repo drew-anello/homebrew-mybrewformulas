5727299414e11a3a9e175ed8e5bc1b6e499a11d5
Drew Anello <drew.anello@gmail.com>
2023-09-10 17:13:28 -0400

    wip

--- Formula/pwg.rb
+++ Formula/pwg.rb
@@ -1,8 +1,8 @@
 class Pwg < Formula
 
   desc "PWG is a random 12 character long password generator"
-  url "https://github.com/drew-anello/random-pw-generator/releases/download/v1.0.0/pwg-v1.0.0.tar.gz"
-  sha256 "21fdd49137e6a2db24c7e65aa8bfb132cdbabd9d8b31f03b60b29a9dbe478ce2"
+ url "https://github.com/drew-anello/random-pw-generator/archive/refs/tags/v1.0.0.tar.gz"
+
 
   depends_on "go" => :build
 
